e43c17986585d896313b4be618c7d0914424905d
fady25 <fadyemad20111@gmail.com>
2024-07-03 20:18:49 +0300

    solve conflict

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+
+const app = express()
+const port = 3000
+
+
+
+
+app.use(express.json())
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log('server is running on port', port);
+})
+
